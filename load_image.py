1306bc5ad8f3f1df78c9a2675eccec120c0197c4
RicardoIST <80824497+RicardoIST@users.noreply.github.com>
2022-12-12 17:47:41 +0000

    Algumas mudanças

--- load_image.py
+++ load_image.py
@@ -8,6 +8,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+#from svg.path import 
 
 class PATH:
     def __init__(self) -> None:
@@ -18,45 +19,80 @@ class PATH:
     ### loads file
     def removepoints(self, file_name: str):
         i=0
-        print(self.points)
         font = cv.FONT_HERSHEY_COMPLEX
         img3 = cv.imread(file_name, cv.IMREAD_COLOR)
         
         j=0
-        while (j<len(self.points)):
+        i=2
+        #self.points.reshape(-1,2)
+        counter=0
+        if(len(self.n_points) == 1): #means that we only have one contour
+            #mediumpoint=int(int(self.n_points[0])/2)+4
+            #self.points=self.points[0:mediumpoint]
+            while (j < int(self.n_points[0]) ):
+                while (i < int(self.n_points[0]) ):
+                    distance = math.sqrt((self.points[j]-self.points[i])**2 + (self.points[j+1]-self.points[i+1])**2)
+                    if distance < 100:
+                        print(distance, self.points[j], self.points[j+1], self.points[i], self.points[i+1])
+                        #self.points=self.points[:i]
+                        counter+=1
+                       
+                        #print(self.points)
+                        #print(len(self.points))
+                    i += 2
+                j+=2
+                i=j+2
+            print(counter)
+            #mediumpoint=int(counter/2)
+            self.points=self.points[:(int(self.n_points[0])-2*counter)]
+
+        """while (j<len(self.points)):
             i=j+2
             while(i<len(self.points) ):
                 distance = np.sqrt((self.points[j]-self.points[i])**2 + (self.points[j+1]-self.points[i+1])**2)
-                if distance < 90:
+                if distance < 100:
                     self.points.pop(i)
                     self.points.pop(i)
                     #print(self.points)
                     #print(len(self.points))
                 else: 
                     i += 2
-            j += 2
+            j += 2"""
         j = 0
         i = 0
+        print(len(self.points))
         #cv.drawContours(img3, self.points, -1 , (0, 255, 0), 2)
-        for j in self.points :
-                if(i % 2 == 0):
-                    x = self.points[i]
-                    y = self.points[i + 1]
+        aux=np.array(self.points).reshape(-1,2)
+        #print(aux)
         
-                    # String containing the co-ordinates.
-                    string = str(x) + " " + str(y) 
         
-                    if(i == 0):
-                        # text on topmost co-ordinate.
-                        cv.putText(img3, "Arrow tip", (x, y),
-                                        font, 1, (255, 0, 0)) 
-                    else:
-                        # text on remaining co-ordinates.
-                        cv.putText(img3, string, (x, y), 
-                                font, 1, (0, 255, 0)) 
-                
-                i = i + 1
-          
+            
+        for i in range(len(self.points)-1):
+            x = aux[i][0]
+            y = aux[i][1]
+            string = str(x) + " " + str(y)
+                # text on remaining co-ordinates.
+            cv.putText(img3, string, (x, y), 
+                    font, 1, (255, 0, 0)) 
+        
+            
+        """for i in range(0,self.n_points[0],2):
+            for j in range(1,len(self.points)):
+                for n in range(self.n_points[j]-1):
+                    distance = np.sqrt((self.points[0][i, 0,0]-self.points[j][n, 0,0])**2 + (self.points[0][i,0,1 ]-self.points[j][n, 0,1])**2)
+                    if distance < 100:
+                        print(self.points[j][n])
+                        self.points[j]=np.concatenate((self.points[j][0:i-1],self.points[j][i+1:self.n_points[j]]), axis=0)
+                        self.n_points[j]-=1
+                        print(self.points[j])
+                        #self.n_points[j]-=2
+           """
+        image = cv.polylines(img3, [aux],
+                      False, (0,255,0), 2)
+        #cv.drawContours(img3, self.points, 1, (255, 0, 0), 2)
+        #cv.drawContours(img3, self.points, 2, (0, 0, 255), 2)      
+        # String containing the co-ordinates.
+        
         plt.imshow(img3)
         #print(self.points[0][0])
         plt.show()
@@ -68,18 +104,22 @@ class PATH:
         imgray = cv.cvtColor(im1, cv.COLOR_BGR2GRAY)
         imgray = cv.GaussianBlur(imgray, (5, 5), 0)
         img2 = cv.imread(file_name, cv.IMREAD_COLOR)
-        ret, thresh = cv.threshold(imgray, 64, 255,cv.THRESH_BINARY_INV)
+        ret, thresh = cv.threshold(imgray, 127, 255,cv.THRESH_BINARY_INV)
+        
         contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        #contours2, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        """if len(contours2)>1:
+            print(contours1)
+            contours1.ravel(contours2[1])"""
         #bmp = potrace.Bitmap(im1)
         #print(bmp.)    
-        ima=cv.drawContours(im1, contours, -1, (0,255,0), 0)
-
+        
         aux = []
+        self.n_points = []
 
         for cnt in contours :
   
-            approx = cv.approxPolyDP(cnt, 0.001*cv.arcLength(cnt, True), True)
-        
+            approx = cv.approxPolyDP(cnt, 0.001*cv.arcLength(cnt, False), True)
             # draws boundary of contours.
             cv.drawContours(img2, [approx], -1, (0, 255, 0), 2) 
             # Used to flatted the array containing
@@ -88,13 +128,13 @@ class PATH:
             i = 0
             
             aux.append(n)
-            
-
+            self.n_points.append(len(n))
+            print(aux)
             for j in n :
                 if(i % 2 == 0):
                     x = n[i]
                     y = n[i + 1]
-        
+
                     # String containing the co-ordinates.
                     string = str(x) + " " + str(y) 
         
@@ -108,7 +148,7 @@ class PATH:
                                 font, 1, (0, 255, 0)) 
                 
                 i = i + 1
-            
+        #self.points=aux
         for sublist in aux:
             for item in sublist:
                 self.points.append(item)
@@ -151,8 +191,8 @@ class PATH:
 #potrace.potrace.CornerSegment
 
 path = PATH()
-path.load_paths_png("images/test_draw_2.png")
-path.removepoints("images/test_draw_2.png")
+path.load_paths_png("images/test_draw_1.png")
+path.removepoints("images/test_draw_1.png")
 #path.generate_arm_positions()
 """ for path in path.paths:
     for segment in path:
