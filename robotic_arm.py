12937ea9bb1f5c83af6cd54930d249b58618b2e8
RicardoIST <80824497+RicardoIST@users.noreply.github.com>
2022-12-27 09:21:36 +0000

    Merge branch 'load_image_move_robot' of https://github.com/andrecosta2000/robotica_lab1 into load_image_move_robot

--- robotic_arm.py
+++ robotic_arm.py
@@ -96,17 +96,17 @@ class RoboticArm:
         print(point3)
         print('######################')
         print('######################')
-        """ self.origin[0]=3963
-        self.origin[1]=-2254
-        self.origin[2]=-873
+        """ self.origin[0]=3698
+        self.origin[1]=-2197
+        self.origin[2]=-635
         point2=np.zeros(3)
-        point2[0]=4679
-        point2[1]=-2217
-        point2[2]=-881
+        point2[0]=4938
+        point2[1]=-2230
+        point2[2]=-660
         point3=np.zeros(3)
-        point3[0]=4344
-        point3[1]=-1383
-        point3[2]=-877 """
+        point3[0]=3984
+        point3[1]=500
+        point3[2]=-595 """
 
         ab=point2-self.origin
         ac=point3-self.origin
@@ -123,10 +123,7 @@ class RoboticArm:
 
     def move_pos_vec(self, name: str, pos_vec: np.array(np.array)):
         self.com_port.write('moves '+name+' '+str(1)+' '+str(pos_vec.shape[0])+'\r')
-        time.sleep(0.5)
-        answer = ''
-        while 'done' not in answer:
-            answer = self.com_port.read_and_wait(0)
+        time.sleep(15)
 
     def run_pos_vector(self, name: str):
         print('move '+name+'\r')
@@ -160,7 +157,7 @@ class RoboticArm:
         time.sleep(0.5)
         self.com_port.read_and_wait(1)
         for i in range(2, pos_vec.shape[0]+1):
-            print('Loading point ', i)
+            print('Loading point ', i,' of ', pos_vec.shape[0])
             #print('here '+name+'['+str(i)+']'+'\r')
             #self.create_pos(name+'['+str(i)+']', pos_vec[i-1][0],pos_vec[i-1][1],pos_vec[i-1][2])
             if pos_vec[i-1][2] == 0:
